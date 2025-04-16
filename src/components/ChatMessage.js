b27a206d309c7db9db2957822a39e15e55b895ee
Kalana Weerakoon <kalana@ursaleo.com>
2025-04-16 14:27:41 +0530

    markdown implementation

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 const ChatMessage = ({ text, type }) => {
   const isUser = type === "user";
   return (
     <div className={`chat-message ${isUser ? "user" : "bot"}`}>
-      <div className="bubble">{text}</div>
+      <div className="bubble">
+        {isUser ? text : <ReactMarkdown>{text}</ReactMarkdown>}
+      </div>
     </div>
   );
 };
